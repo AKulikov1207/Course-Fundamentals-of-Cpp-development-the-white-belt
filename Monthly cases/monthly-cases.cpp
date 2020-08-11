21c2791c7a329e7cefcc745a58dd2b65290ef695
kulya_228 <kandrey1207@mail.ru>
2020-08-11 16:33:43 +0300

    Added comments to .cpp file

--- Monthly cases/monthly-cases.cpp
+++ Monthly cases/monthly-cases.cpp
@@ -9,8 +9,11 @@ int main()
 	int day = 0;
 	string command;
 	int Q;
-	const vector<int> days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	vector<vector<string>> todo(days[day]);
+	const vector<int> days_months = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	
+        // внешний вектор должен иметь длину, равную количеству дней в первом месяце;
+        // все внутренние векторы по умолчанию пусты, потому что дел изначально нет
+	vector<vector<string>> todo(days_months[day]);
 
 	cin >> Q;
 
@@ -23,20 +26,17 @@ int main()
 
 			cin >> d >> deal;
 			todo[d - 1].push_back(deal);
-
-			//continue;
 		}
 
 		if (command == "DUMP") {
 			int d;
 			cin >> d;
-
+			
+			// выводим список дел в конкретный день в нужном формате
 			cout << todo[d - 1].size() << " ";
 			for (auto w : todo[d - 1])
 				cout << w << " ";
 			cout << endl;
-
-			//continue;
 		}
 
 		if (command == "NEXT") {
@@ -44,7 +44,7 @@ int main()
 			if (++day > 11)
 				day = 0;
 
-			todo.resize(days[day]);
+			todo.resize(days_months[day]);
 
 			if (todo.size() < buf.size())
 			{
@@ -53,8 +53,6 @@ int main()
 					todo[todo.size() - 1].insert(end(todo[todo.size() - 1]), begin(buf[i]), end(buf[i]));
 				}
 			}
-
-			//continue;
 		}
 		
 		--Q;
